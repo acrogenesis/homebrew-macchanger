509d2fdc88b8743b2678cc224e3bfe05e9892b31
Adrian Rangel <adrian.rangel@gmail.com>
2018-04-12 21:15:20 -0500

    update sha256

--- macchanger.rb
+++ macchanger.rb
@@ -4,7 +4,7 @@ class Macchanger < Formula
   homepage "https://github.com/acrogenesis/macchanger"
   version "1.3"
   url "https://github.com/acrogenesis/macchanger/archive/1.3.tar.gz"
-  sha256 "dc80b1b9fa07206442d2ecdb9306cd2304996bbb056142ca91b6350cecb0157b"
+  sha256 "09f3e9238653f7adb4da396466f92f3c7d4ea04e0814480f5abb22b0a1d1d3b3"
 
   def install
     bin.install "bin/macchanger"
