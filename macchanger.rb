e718d41a455f00adf4597ada1edb468c66147bd8
Adrian Rangel <adrian.rangel@gmail.com>
2018-04-12 21:17:42 -0500

    update 1.3.1

--- macchanger.rb
+++ macchanger.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Macchanger < Formula
   homepage "https://github.com/acrogenesis/macchanger"
-  version "1.3"
-  url "https://github.com/acrogenesis/macchanger/archive/1.3.tar.gz"
-  sha256 "09f3e9238653f7adb4da396466f92f3c7d4ea04e0814480f5abb22b0a1d1d3b3"
+  version "1.3.1"
+  url "https://github.com/acrogenesis/macchanger/archive/v1.3.1.tar.gz"
+  sha256 "ed7ee0fc14068316244e94741bdd195b76627518b0b004f6fd548518509be590"
 
   def install
     bin.install "bin/macchanger"
