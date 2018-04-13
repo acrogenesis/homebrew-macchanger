afdd87e1a81646f0dd7333cf42ce4bfcbc08e5a4
Adrian Rangel <adrian.rangel@gmail.com>
2018-04-12 21:14:48 -0500

    update to correct url

--- macchanger.rb
+++ macchanger.rb
@@ -3,7 +3,7 @@ require "formula"
 class Macchanger < Formula
   homepage "https://github.com/acrogenesis/macchanger"
   version "1.3"
-  url "https://github.com/acrogenesis/macchanger/archive/v1.3.tar.gz"
+  url "https://github.com/acrogenesis/macchanger/archive/1.3.tar.gz"
   sha256 "dc80b1b9fa07206442d2ecdb9306cd2304996bbb056142ca91b6350cecb0157b"
 
   def install
