4da7bbc4f18a79711c9827dfb825544753dd9f98
Adrian Rangel <adrian.rangel@gmail.com>
2025-06-30 12:45:26 -0600

    Update macchanger.rb 1.6.0

--- macchanger.rb
+++ macchanger.rb
@@ -4,7 +4,7 @@ class Macchanger < Formula
   homepage "https://github.com/acrogenesis/macchanger"
   version "1.6.0"
   url "https://github.com/acrogenesis/macchanger/archive/v1.6.0.tar.gz"
-  sha256 "043c58e70bc7d76c44a7a042c677d4665be7de3e25392cb1009ed6285490aeae"
+  sha256 "c7e70a8d432f9b7e5a6c6a8162e99dfa506e86e0ab65c18244f47f222b5f5bd4"
 
   def install
     bin.install "bin/macchanger"
